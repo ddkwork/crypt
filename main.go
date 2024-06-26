c67ab056a3e33aa72f57e38e477c7eeae7f9d3dd
Admin <2762713521@qq.com>
2024-06-26 20:26:15 +0800

    start remove core code

--- main.go
+++ main.go
@@ -26,12 +26,7 @@ import (
 	//
 )
 
-//go:generate core generate
-//go:generate core build -v -t android/arm64
-//go:generate core build -v -t windows/amd64
-//go:generate go build .
 //go:generate go install .
-//go:generate svg embed-image 2.png
 
 //go:embed 2.svg
 var icon []byte
