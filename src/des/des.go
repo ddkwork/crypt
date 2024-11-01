91a3eb3b88b2014198cbccbaa66e1750ac4e013e
Admin <2762713521@qq.com>
2024-11-01 15:54:27 +0800

    init

--- src/des/des.go
+++ src/des/des.go
@@ -1,12 +1,27 @@
 package des
 
 import (
+	"github.com/ddkwork/golibrary/mylog"
 	"github.com/ddkwork/golibrary/stream"
+)
+
+var (
+	encryptCount = 0
+	decryptCount = 0
 )
 
 func Encrypt[T stream.Type](src, key T) (dst *stream.Buffer) {
 	s := stream.NewBuffer(src)
 	k := stream.NewBuffer(key)
+	encryptCount++
+	defer func() {
+		mylog.Struct(EncryptInfo{
+			Src:   s.Bytes(),
+			Key:   k.Bytes(),
+			Data:  dst.Bytes(),
+			Count: encryptCount,
+		})
+	}()
 	s.CheckDesBlockSize()
 	k.CheckDesBlockSize()
 	subKeys := expand(k.Bytes())
@@ -16,8 +31,37 @@ func Encrypt[T stream.Type](src, key T) (dst *stream.Buffer) {
 func Decrypt[T stream.Type](src, key T) (dst *stream.Buffer) {
 	s := stream.NewBuffer(src)
 	k := stream.NewBuffer(key)
+	decryptCount++
+	defer func() {
+		mylog.Struct(DecryptInfo{
+			Src:   s.Bytes(),
+			Key:   k.Bytes(),
+			Data:  dst.Bytes(),
+			Count: decryptCount,
+		})
+	}()
 	s.CheckDesBlockSize()
 	k.CheckDesBlockSize()
 	subKeys := expand(k.Bytes())
 	return stream.NewBuffer(des_decrypt(s.Bytes(), subKeys))
+}
+
+type DecryptInfo struct {
+	Src   []byte
+	Key   Key
+	Data  []byte
+	Count int
+}
+
+type EncryptInfo struct {
+	Src   []byte
+	Key   Key
+	Data  []byte
+	Count int
+}
+
+type Key []byte
+
+func (k Key) String() string { //让mylog.Struct统一慢慢反射，一劳永逸，这就是接口的力量
+	return string(k)
 }
