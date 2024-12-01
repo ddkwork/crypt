9c1f35e4b18bd365ec254585f72c72e8b44e10fd
Admin <2762713521@qq.com>
2024-12-01 18:10:45 +0800

    fix https://github.com/goradd/maps/issues/15

--- main/CryptUI.go
+++ main/CryptUI.go
@@ -126,7 +126,7 @@ func (c *CryptUI) Layout() *unison.Panel {
 	widget.SetScrollLayout(splitPanel, 2)
 
 	left := widget.NewTableScrollPanel(table, header)
-	layouts := new(safemap.SafeMap[CryptNameType, func() unison.Paneler])
+	layouts := new(safemap.M[CryptNameType, func() unison.Paneler])
 	layouts.Set(AesType, func() unison.Paneler {
 		view, RowPanel := widget.NewStructView(SrcKeyDstdData{}, func(data SrcKeyDstdData) (values []widget.CellData) {
 			return []widget.CellData{{Text: data.Src}, {Text: data.Key}, {Text: data.Dst}}
